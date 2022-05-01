f9eb6d3031a3f69468a12c00bfeb769a383eefbb
sumiya12 <sumiya.udval@gmail.com>
2022-05-01 21:46:17 +0800

    next prev page

--- src/js/view/searchView.js
+++ src/js/view/searchView.js
@@ -23,6 +23,36 @@ export const clearSearchResult =() => {
     elements.searchResultList.innerHTML= '';
 }
 export const getInput = () => elements.searchInput.value;
-export const renderRecipes = recipes => {
-    recipes.forEach(el => renderRecipe(el));
+export const renderRecipes =( recipes ,currentPage = 1, resPerPage = 10) => {
+// hailtiin ur dung huudaslaj vzvvleh
+    const start = (currentPage - 1 ) * resPerPage;
+    const end = currentPage * resPerPage;
+    recipes.slice(start,end).forEach(renderRecipe)
+    // huudaslaltiin tovchiig gargah
+    const totalPages = Math.ceil(recipes.length / resPerPage);
+    renderButtons(currentPage , totalPages);
 }
+const createButton = (page ,type , direction ) => `
+<button class="btn-inline results__btn--${type}">
+<span>Хуудас ${page}</span>
+<svg class="search__icon">
+    <use href="img/icons.svg#icon-triangle-${direction}"></use>
+</svg>
+</button>`;
+
+const renderButtons = (currentPage , totalPages)=>{
+    let buttonHTML ;
+    if(currentPage === 1 && totalPages > 1){
+        // 1r huudsan deer bn 2 r huudas gedeg tovchiig gargah estoi
+        buttonHTML = createButton(2, 'next' ,"right")
+    }else if(currentPage < totalPages ){
+        // umnuh ruu bolon daraachiin huudsiig tovchiig vzvvlne
+        buttonHTML = createButton(currentPage - 1,'prev',"left")
+        buttonHTML += createButton(currentPage + 1,'next',"right")
+    }
+    else if(currentPage === totalPages ){
+        // hamgiin suuliin huudas deer bn umnuh ruu tovchiig vzvvlne
+        buttonHTML = createButton(currentPage - 1, 'prev', "left")
+    }
+    elements.pageButtons.insertAdjacentHTML('afterbegin', buttonHTML)
+};
