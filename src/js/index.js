abf6c7a01062481d789b6b81fafff8c4ba4bf38f
sumiya12 <sumiya.udval@gmail.com>
2022-05-01 17:44:13 +0800

    searchview result

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,33 @@
 require("@babel/polyfill");
 import Search from "./model/Search";
+import { elements } from "./view/base";
+import * as searchView from "./view/searchView";
+/**
+ * web app төлөв
+ * хайлтын күэрийь үр дүн
+ * тухайн үзүүлж байгаа жор
+ * захиалж байгаа жорын найрлаганууд
+ */
+const state = {};
 
-let search = new Search("pasta");
+const controlSearch = async () => {
+  // 1 вэбээс хайлтын түлхүүр үгийг гаргаж авна
+  const query = searchView.getInput();
+  if (query) {
+    // шинээр хайлтын объектйг үүсгэж өгнө
+    state.search = new Search(query);
+    // хайлт хийхэд зориулж дэлгэцийг бэлтгэж өгнө.
+    searchView.clearSearch();
+    searchView.clearSearchResult();
+    //  хайлтыг гүйцэтгэнэ
+    await state.search.doSearch();
+    // хайлтын үр дүнг дэлгэцэнд үзүүлнэ
+    if (state.search.result === undefined) alert("Хайлтаар илэрцгүй ...");
+    else searchView.renderRecipes(state.search.result);
+  }
+};
 
-search.doSearch().then(r => console.log(r));
+elements.searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  controlSearch();
+});
